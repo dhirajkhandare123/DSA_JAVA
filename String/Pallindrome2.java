f0e72d84911825853302515281f3a97cc42db3bb
Dkhandare123 <dhirajkhandare76@gmail.com>
2026-08-11 09:09:31 +0530

    Create Pallindrome2.java

---
+++ String/Pallindrome2.java
@@ -0,0 +1,21 @@
+public class Pallindrome2 {
+    public static void main(String[] args) {
+        String str = "Levell";
+
+        System.out.println(isPallindrome(str));
+    }
+
+    public static boolean isPallindrome(String str){
+        String lowerStr = str.toLowerCase();
+        String rev="";
+
+        for(int i=lowerStr.length()-1;i>=0;i--){
+            rev=rev+lowerStr.charAt(i);
+        }
+
+        if(lowerStr.equals(rev)){
+            return true;
+        }
+        return false;
+    }
+}
