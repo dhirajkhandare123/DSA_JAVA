0662c00f5ba9786b4cfbec567fa4dfeca49baaee
Dkhandare123 <dhirajkhandare76@gmail.com>
2026-08-11 09:09:28 +0530

    Create Pallindrome.java

---
+++ String/Pallindrome.java
@@ -0,0 +1,24 @@
+
+
+public class Pallindrome{
+    public static void main(String[] args) {
+        String str = "level";
+
+        System.out.println(isPallindrome(str));
+    }
+
+    public static boolean isPallindrome(String s){
+        String s1 = s;
+
+        StringBuilder sb = new StringBuilder(s1);
+
+        sb.reverse();
+
+        if(s1.equals(sb.toString())){
+            return true;
+        }
+
+
+        return false;
+    }
+}
