c41dde1a5e6bc0993b6f830746b41086c014b643
Dkhandare123 <dhirajkhandare76@gmail.com>
2026-08-11 09:09:13 +0530

    Create CountNumberOfCharacters.java

---
+++ String/CountNumberOfCharacters.java
@@ -0,0 +1,17 @@
+public class CountNumberOfCharacters {
+    public static void main(String[] args) {
+        String str= "Hello World";
+
+        System.out.println(countCharachters(str));
+    }
+
+    public static int countCharachters(String str){
+        int count = 0;
+        for(int i=0;i<=str.length()-1;i++){
+            if(str.charAt(i)!=' '){
+                count++;
+            }
+        }
+        return count;
+    }
+}
