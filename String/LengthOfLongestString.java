4b9777ca1e35da58cb4b1784c3e954994f168550
Dkhandare123 <dhirajkhandare76@gmail.com>
2026-08-11 10:04:43 +0530

    Create LengthOfLongestString.java

---
+++ String/LengthOfLongestString.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class LengthOfLongestString {
+
+    public static void main(String[] args) {
+        String str= "abcdabcebb";
+        int start = 0;
+        int end = 0;
+        int max_lenght=0;
+
+        List<Character>list = new ArrayList<>();
+
+        while(end!=str.length()){
+            if(!list.contains(str.charAt(end))){
+                list.add(str.charAt(end));
+                end++;
+                max_lenght=Math.max(max_lenght,list.size());
+            }
+            else{
+                list.remove(Character.valueOf(str.charAt(start)));
+                start++;
+            }
+        }
+
+        System.out.println("max length: "+max_lenght);
+    }
+}
